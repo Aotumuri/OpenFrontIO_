a4f19cd31aa7605ef34c7fa10d33be97870ac41e
Aotumuri <aotumuri.yt@gmail.com>
2025-03-28 15:58:32 +0900

    .w.

--- src/core/game/Stats.ts
+++
@@ -1,8 +0,0 @@
-import { AllPlayersStats, PlayerStats } from "../Schemas";
-import { NukeType, PlayerID } from "./Game";
-
-export interface Stats {
-  increaseNukeCount(sender: PlayerID, target: PlayerID, type: NukeType): void;
-  getPlayerStats(player: PlayerID): PlayerStats;
-  stats(): AllPlayersStats;
-}
